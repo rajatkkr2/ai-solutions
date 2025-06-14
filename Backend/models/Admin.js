e99941f87848a6320d8deafa26ed030100b1a4f9
rajatkkr2 <74487472+rajatkkr2@users.noreply.github.com>
2025-06-14 14:13:02 +0530

    auth and admin changes (#5)

---
+++ Backend/models/Admin.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+
+const AdminSchema = new mongoose.Schema({
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+});
+
+// Hash password before saving
+AdminSchema.pre("save", async function(next) {
+  if (!this.isModified("password")) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+// Method to compare password
+AdminSchema.methods.matchPassword = function(password) {
+  return bcrypt.compare(password, this.password);
+};
+
+module.exports = mongoose.model("logins", AdminSchema);
